140b8b50f50cc14216df250aa08220e1ff017a1e
Bernd Lehmann <bernd@lehmann.be>
2018-07-01 15:19:03 +0200

    changed variables

--- variables.tf
+++ variables.tf
@@ -35,21 +35,21 @@ variable "public_subnet_a" {
 }
 variable "public_subnet_b" {
   default = "10.0.4.0/24"
-  description = "Public subnet AZ A"
+  description = "Public subnet AZ B"
 }
 variable "public_subnet_c" {
   default = "10.0.8.0/24"
-  description = "Public subnet AZ A"
+  description = "Public subnet AZ C"
 }
 variable "private_subnet_a" {
   default = "10.0.1.0/24"
-  description = "Public subnet AZ A"
+  description = "Private subnet AZ A"
 }
 variable "private_subnet_b" {
   default = "10.0.5.0/24"
-  description = "Public subnet AZ A"
+  description = "Private subnet AZ B"
 }
 variable "private_subnet_c" {
   default = "10.0.9.0/24"
-  description = "Public subnet AZ A"
+  description = "Private subnet AZ C"
 }
